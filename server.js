6a3e611d3ed7b0e227cd5bcad17a55bf46cccb83
justinclark-dev <justinclark.dev@gmail.com>
2025-10-27 11:58:36 -0700

    adds ejs templating

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const dotenv = require('dotenv');
-dotenv.config();
-// dotenv.config({ quiet: true });
+dotenv.config({ quiet: true });
 
 const express = require('express');
 const app = express();
+
+// GET /
+app.get('/', (req, res) => {
+    res.render('index.ejs', {
+        page: { title: 'Home' }
+    })
+});
+
+
+// app.get('/', (req, res) => {
+//     res.render('index.ejs', { 
+//         page: { title: 'Welcome to my blog!' }
+//     });
+// });
+
 
 app.listen('3000', () => {
     console.log(`Listening on port 3000!`);
